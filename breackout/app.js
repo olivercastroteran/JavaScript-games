21e19b3c8ffb43a2164ef39f78de5fbc33248258
olivercastroteran <olivercastroteran@gmail.com>
2020-06-30 12:51:53 -0400

    breackout - initial UI build plus show and hide rules functionality added

---
+++ breackout/app.js
@@ -0,0 +1,13 @@
+// DOM elements
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
+
+// Event listeners
+rulesBtn.addEventListener('click', () => {
+  rules.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  rules.classList.remove('show');
+});
