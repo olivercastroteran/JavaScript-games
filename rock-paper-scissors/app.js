163bcd6f6efaccf4e15ff898a462fb712b48e93c
olivercastroteran <olivercastroteran@gmail.com>
2020-07-05 12:24:18 -0400

    rock paper scissors - checking for the winner

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -28,8 +28,55 @@ const game = () => {
         // the computer choice
         const computerNumber = Math.floor(Math.random() * 3);
         const computerChoice = computerOptions[computerNumber];
+
+        // Call compare hands/choices
+        console.log(`player: ${this.innerHTML}`, `computer: ${computerChoice}`);
+        compareHands(this.innerHTML, computerChoice);
       });
     });
+  };
+
+  // compare player and computer choices
+  const compareHands = (playerChoice, computerChoice) => {
+    // Update Text
+    const winner = document.querySelector('.winner');
+    if (playerChoice === computerChoice) {
+      winner.textContent = 'It is a tie';
+      return;
+    }
+
+    // Choose rock
+    if (playerChoice === 'rock') {
+      if (computerChoice === 'scissors') {
+        winner.textContent = 'Player Wins';
+        return;
+      } else {
+        winner.textContent = 'Compuer Wins';
+        return;
+      }
+    }
+
+    // Choose paper
+    if (playerChoice === 'paper') {
+      if (computerChoice === 'scissors') {
+        winner.textContent = 'Computer Wins';
+        return;
+      } else {
+        winner.textContent = 'Player Wins';
+        return;
+      }
+    }
+
+    // Choose scissors
+    if (playerChoice === 'scissors') {
+      if (computerChoice === 'paper') {
+        winner.textContent = 'Player Wins';
+        return;
+      } else {
+        winner.textContent = 'Compuer Wins';
+        return;
+      }
+    }
   };
 
   // Call all the inner functions
