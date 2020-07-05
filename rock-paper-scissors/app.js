e9a26e5bbe437a74f913b325d6d91ef4800b7dc1
olivercastroteran <olivercastroteran@gmail.com>
2020-07-05 12:06:21 -0400

    rock paper scissors - saving player and computer choices

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -14,8 +14,27 @@ const game = () => {
     });
   };
 
+  // Play Match
+  const playMatch = () => {
+    const options = document.querySelectorAll('.options button');
+    const playerHand = document.querySelector('.player-hand');
+    const computerHand = document.querySelector('.computer-hand');
+
+    // Computer Options
+    const computerOptions = ['rock', 'paper', 'scissors'];
+
+    options.forEach((option) => {
+      option.addEventListener('click', function () {
+        // the computer choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+      });
+    });
+  };
+
   // Call all the inner functions
   startGame();
+  playMatch();
 };
 
 // Start the game
