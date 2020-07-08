875d52f81e85fe1387c1762ef54496664001c7fb
olivercastroteran <olivercastroteran@gmail.com>
2020-07-08 18:12:46 -0400

    snake game - controllin update game speed base on snake speed variable

---
+++ snake-game/game.js
@@ -0,0 +1,14 @@
+let lastRenderTime = 0;
+const SNAKE_SPEED = 1;
+
+function main(currentTime) {
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  console.log('Render');
+  lastRenderTime = currentTime;
+}
+
+window.requestAnimationFrame(main);
