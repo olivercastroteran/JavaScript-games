5c76df650ae6e80ba72ebe4609eb3396bc65c3d9
olivercastroteran <olivercastroteran@gmail.com>
2020-07-09 11:31:02 -0400

    snake game - make snake grow on food catching

--- snake-game/food.js
+++ snake-game/food.js
@@ -1,7 +1,15 @@
-let food = { x: 1, y: 1 };
+import { onSnake, expandSnake } from './snake.js';
+import { getRandomGridPosition } from './grid.js';
+
+let food = getRandomFoodPosition();
 const EXPANSION_RATE = 1;
 
-export function update() {}
+export function update() {
+  if (onSnake(food)) {
+    expandSnake(EXPANSION_RATE);
+    food = getRandomFoodPosition();
+  }
+}
 
 export function draw(gameBoard) {
   const foodElement = document.createElement('div');
@@ -11,4 +19,14 @@ export function draw(gameBoard) {
   foodElement.classList.add('food');
 
   gameBoard.appendChild(foodElement);
+}
+
+function getRandomFoodPosition() {
+  let newFoodPosition;
+
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = getRandomGridPosition();
+  }
+
+  return newFoodPosition;
 }
