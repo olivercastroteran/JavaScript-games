532ca554478b0101b07787ea606c94dc3ec92a40
olivercastroteran <olivercastroteran@gmail.com>
2020-07-08 21:32:53 -0400

    snake game - setting up conditions to move the snake

---
+++ snake-game/food.js
@@ -0,0 +1,14 @@
+let food = { x: 1, y: 1 };
+const EXPANSION_RATE = 1;
+
+export function update() {}
+
+export function draw(gameBoard) {
+  const foodElement = document.createElement('div');
+
+  foodElement.style.gridRowStart = food.y;
+  foodElement.style.gridColumnStart = food.x;
+  foodElement.classList.add('food');
+
+  gameBoard.appendChild(foodElement);
+}
